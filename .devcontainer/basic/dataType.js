0b370e9a42d54a739578e1bbe15b0a3a652a9ca5
nittioo7 <128901027+nittioo7@users.noreply.github.com>
2025-04-05 14:21:14 +0000

    var, const and dataType

---
+++ .devcontainer/basic/dataType.js
@@ -0,0 +1,33 @@
+//dataType
+
+"use strict" //treat all JS code as newer version
+
+// will run browser
+//alert("hello" + 3+3) 
+
+
+console.log(3+3);
+// documentation - officail - tc39, ecma and mdn
+
+let name = "nitesh"
+
+//number => 2 to power 53
+// bitint
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undefined => not assiened value 
+//symbol => unique => react to find uniqueness and used in figma
+
+// object
+
+console.log(typeof name);
+console.log(typeof null);//object
+console.log(typeof undefined);// undefined
+
+
+
+
+
+
+
---
+++ .devcontainer/basic/varAndConst.js
@@ -0,0 +1,21 @@
+// var const
+
+const accountId = 1842
+let accountEmail = "Nitesh@nk.con"
+var accountPassword = "niteshK"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2 //not allowed
+
+accountEmail = "nit@k.in"
+accountPassword = 3445
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and function scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]); // not mandatory to use simeclone
